9194e879e10b0895ee10616077ab2f7a9d92a446
api1234 <apnonie@gmail.com>
2018-02-06 11:14:55 -0800

    add key schema post

--- schema_post.py
+++ schema_post.py
@@ -9,6 +9,11 @@ value_fp = open(value_location)
 schema_base_url = 'http://schema_registry:8081/'
 print(schema_base_url)
 
+key_resp = requests.post(schema_base_url+'subjects/{}-key/versions'.format(topic),
+			 	json={'schema': key_fp.read() })
+
+print(key_resp.json())
+
 value_resp = requests.post(schema_base_url+'subjects/{}-value/versions'.format(topic),
 				json={'schema': value_fp.read() })
 print(value_resp.json())
